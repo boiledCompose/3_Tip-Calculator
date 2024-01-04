7207a550094aae2aeb3ba974e7a1255cbd268cb0
Seokjun Lee <seokjun2000@konkuk.ac.kr>
2024-01-04 10:07:34 +0900

    Auto-Test Included

---
+++ app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -0,0 +1,19 @@
+package com.example.tiptime
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.text.NumberFormat
+import java.util.Locale
+
+class TipCalculatorTests {
+
+    @Test
+    fun calculateTip_20PercentNoRoundup(){
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance(Locale.US).format(2)
+        val actualTip = calculateTip(amount, tipPercent, false)
+
+        assertEquals(expectedTip, actualTip)
+    }
+}
